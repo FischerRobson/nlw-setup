90e8650f042b17839d1fd8512aeb8d66d27f921d
Fischer <fischerrobson@gmail.com>
2023-01-19 23:09:42 -0300

    header component

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.tsx ',
+    './src/**/*.tsx',
     './index.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#09090A'
+      }
+    },
   },
   plugins: [],
 }
