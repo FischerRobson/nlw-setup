b08bfe5ace4e7691222a5dcc1a2dc860d468da35
Fischer <fischerrobson@gmail.com>
2023-01-26 22:53:02 -0300

    navigation implemented on mobile

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,9 +1,13 @@
 import { View, TouchableOpacity, Text } from "react-native";
 import Logo from "../assets/logo.svg";
 import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 import colors from "tailwindcss/colors";
 
 export function Header() {
+
+  const { navigate } = useNavigation();
+
   return (
     <View className="w-full flex-row items-center justify-between">
       <Logo />
@@ -11,6 +15,7 @@ export function Header() {
       <TouchableOpacity
         className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
         activeOpacity={.7}
+        onPress={() => navigate("new")}
       >
         <Feather name="plus" color={colors.violet[500]} size={20} />
         <Text className="text-white ml-3 font-semibold text-base">
